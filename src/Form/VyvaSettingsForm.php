ce0a041d3a2070eef518aa749866748fde279194
Andrew Berezovsky <andrew.berezovsky@gmail.com>
2021-03-23 17:58:23 +0800

    [VYVA-8] Added Vimeo authentication settings saving into Drupal state.

--- src/Form/VyvaSettingsForm.php
+++ src/Form/VyvaSettingsForm.php
@@ -4,11 +4,28 @@ namespace Drupal\vyva\Form;
 
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Provide the settings form for Virtual Y Video automation.
  */
 class VyvaSettingsForm extends ConfigFormBase {
+
+  /**
+   * The state store.
+   *
+   * @var \Drupal\Core\State\StateInterface
+   */
+  protected $state;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    $instance = parent::create($container);
+    $instance->state = $container->get('state');
+    return $instance;
+  }
 
   /**
    * {@inheritdoc}
@@ -63,6 +80,29 @@ class VyvaSettingsForm extends ConfigFormBase {
       '#title' => $this->t('Token'),
     ];
 
+    $form['vimeo'] = [
+      '#type' => 'fieldset',
+      '#title' => $this->t('Vimeo settings'),
+    ];
+
+    $form['vimeo']['client_id'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Client ID'),
+      '#default_value' => $this->state->get('vyva.vimeo.client_id'),
+    ];
+
+    $form['vimeo']['client_secret'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Client Secret'),
+      '#default_value' => $this->state->get('vyva.vimeo.client_secret'),
+    ];
+
+    $form['vimeo']['access_token'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Access Token'),
+      '#description' => $this->t('We do not show the saved value here for security reasons.'),
+    ];
+
     return parent::buildForm($form, $form_state);
   }
 
@@ -70,10 +110,11 @@ class VyvaSettingsForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    // TODO: add submit logic.
-    $values = $form_state->getValues();
+    $this->state->set('vyva.vimeo.client_id', $form_state->getValue('client_id'));
+    $this->state->set('vyva.vimeo.client_secret', $form_state->getValue('client_secret'));
+    $this->state->set('vyva.vimeo.access_token', $form_state->getValue('access_token'));
+
     parent::submitForm($form, $form_state);
-    return $values;
   }
 
 }
